4f21aef570342ab8f03c5407cad2bb1d19198ce5
sudarikov <alexandrossudarikov@yandex.ru>
2024-09-03 11:50:08 +0300

    sprint_0

---
+++ sprint_0/first_symbol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string first_word, second_word, third_word, fourth_word, fifth_word;
+    cin >> first_word >> second_word
+    >> third_word >> fourth_word
+    >> fifth_word;
+
+    cout << first_word[0] << second_word[0]
+    << third_word[0] << fourth_word[0] << fifth_word[0] << endl;
+}
