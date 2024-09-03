4d1e7e2fe65f5a0f957f5d19771e0b2066f7b811
Alexander Sudarikov <123568270+my1lane@users.noreply.github.com>
2024-09-03 11:52:07 +0300

    sprint_0

--- sprint_0/first_symbol.cpp
+++ sprint_0/first_symbol.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     string first_word, second_word, third_word, fourth_word, fifth_word;
+    
     cin >> first_word >> second_word
     >> third_word >> fourth_word
     >> fifth_word;
